d3a861d1174bfce7f0abde5cd5f1c3f293529b24
ChiThanhTran <89621197+ChiThanhTran@users.noreply.github.com>
2022-02-16 01:53:08 +0700

    Add files via upload

---
+++ Models/Person.cs
@@ -0,0 +1,53 @@
+namespace NETMVCDAY1.Models;
+using System.ComponentModel.DataAnnotations;
+public class Person : IComparable
+{
+        public int Id{get;set;}
+        [Required, MaxLength(50)]
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public string Gender {get;set;}
+        public DateTime DateOfBirth {get;set;}
+        public string PhoneNumber {get;set;}
+        public string BirthPlace {get;set;}
+        public int Age 
+        {
+            get 
+            {
+                return DateTime.Now.Year - DateOfBirth.Year;
+            }
+        }
+        public bool IsGraduated {get;set;}
+        public int TotalDays 
+        {
+            get  
+            {
+                return (int)(DateTime.Now - DateOfBirth).TotalDays;
+            }
+        }
+        public int CompareTo(object? obj)
+        {
+            return TotalDays.CompareTo(((Person)obj).TotalDays);
+        }
+        public string FullName 
+        {
+            get
+            {
+                return LastName+" "+FirstName;
+            }
+        }    
+
+}
+public class PersonEditModel : Person{
+    
+    public PersonEditModel(){}
+    public PersonEditModel(Person person){
+        FirstName = person.FirstName;
+        LastName = person.LastName;
+        Gender =    person.Gender;
+        DateOfBirth = person.DateOfBirth;
+        PhoneNumber = person.PhoneNumber;
+        BirthPlace = person.BirthPlace;
+    }
+    public int Index{get;set;}
+}
